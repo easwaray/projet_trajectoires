c18f965e6f575ee073fbd3c269809953abab21c7
easwaray <40615078+easwaray@users.noreply.github.com>
2018-07-25 00:48:23 +0200

    Update CN_3D.m

--- src/Plaque_plane/CN_3D.m
+++ src/Plaque_plane/CN_3D.m
@@ -1,6 +1,6 @@
 function [spline_Cn]=CN_3D(q1,q2,q3,q4,u,v,w,vf,vyf,wf,Vq)
 
-Ub=Rq(q1,q2,q3,q4)*[vf-u;vyf-v;wf-w];
+Ub=Matrice_de_passage(q1,q2,q3,q4)*[vf-u;vyf-v;wf-w];
 alpha_local=asind(Ub(3)/(sqrt(Ub(1).^2+Ub(2).^2+Ub(3).^2)+1e-12));
 beta_local=asind(Ub(2)/(sqrt(Ub(1).^2+Ub(2).^2+Ub(3).^2)*cosd(alpha_local)+1e-12));
 
