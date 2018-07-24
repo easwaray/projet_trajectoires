770899169f08c740531f447a12e2f908a0591847
easwaray <40615078+easwaray@users.noreply.github.com>
2018-07-25 00:50:25 +0200

    Update Moment_plaque_plane_Tachikawa.m

--- src/Plaque_plane/Moment_plaque_plane_Tachikawa.m
+++ src/Plaque_plane/Moment_plaque_plane_Tachikawa.m
@@ -1,21 +1,21 @@
 function [Mb]=Moment_plaque_plane_Tachikawa( L,l,e,P,Q,R,q1,q2,q3,q4,rhoair,u,v,w,vf,vyf,wf,Vq)
 
 
-%vitesse dans le repère de la plaque
-Ub=Rq(q1,q2,q3,q4)*[vf-u;vyf-v;wf-w];
+%vitesse dans le repÃ¨re de la plaque
+Ub=Matrice_de_passage(q1,q2,q3,q4)*[vf-u;vyf-v;wf-w];
 
 %centre de pression :
 C_p=centre_pression(q1,q2,q3,q4,u,v,w,vf,vyf,wf,L,l);
 
-%Force apliquée au centre de pression : 
-Fb=Rq(q1,q2,q3,q4)*Forces_plaque_plane(q1,q2,q3,q4,L,l,rhoair,u,v,w,vf,vyf,wf,Vq);
+%Force apliquÃ©e au centre de pression : 
+Fb=Matrice_de_passage(q1,q2,q3,q4)*Forces_plaque(q1,q2,q3,q4,L,l,rhoair,u,v,w,vf,vyf,wf,Vq);
 
-%Moment de la force appliquée au centre de pression :
+%Moment de la force appliquÃ©e au centre de pression :
 Mf=cross(C_p,Fb);
 
 %Damping moment (moment d'amortissement)
 if L/l == 1
-W0=0.64*sqrt(Ub(1)^2+Ub(2)^2+Ub(3)^2)/L; %W0 = 0.32*2V/L plaque carrée
+W0=0.64*sqrt(Ub(1)^2+Ub(2)^2+Ub(3)^2)/L; %W0 = 0.32*2V/L plaque carrÃ©e
 W1=0.64*sqrt(Ub(1)^2+Ub(2)^2+Ub(3)^2)/L;
 end
 if L/l == 2
@@ -29,5 +29,5 @@ Md = 0.5*rhoair*(Ub(1)^2+Ub(2)^2+Ub(3)^2)*LSCmr;
 
 %Moment total
 
-Mb=Mf+Md; % Mf correspond à Mp dans le mémoire 
+Mb=Mf+Md; % Mf correspond Ã  Mp dans le mÃ©moire 
 end
