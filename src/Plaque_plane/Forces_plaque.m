361c871add711d588445c9bc7db3298cc6ed2f51
easwaray <40615078+easwaray@users.noreply.github.com>
2018-07-25 00:49:07 +0200

    Update Forces_plaque.m

--- src/Plaque_plane/Forces_plaque.m
+++ src/Plaque_plane/Forces_plaque.m
@@ -1,18 +1,18 @@
-function [Fi] = Forces_plaque_plane(q1,q2,q3,q4,L,l,rhoair,u,v,w,vf,vyf,wf,Vq)
+function [Fi] = Forces_plaque(q1,q2,q3,q4,L,l,rhoair,u,v,w,vf,vyf,wf,Vq)
 
-%vitesse relative dans le référentiel mobile de la plaque
+%vitesse relative dans le rÃ©fÃ©rentiel mobile de la plaque
 
-Ub=Rq(q1,q2,q3,q4)*[vf-u;vyf-v;wf-w];
+Ub=Matrice_de_passage(q1,q2,q3,q4)*[vf-u;vyf-v;wf-w];
 
-%forces dans le référentiel mobile de la plaque
-%hypothèse  : seule la force normale est considérée comme prépondérante sur
+%forces dans le rÃ©fÃ©rentiel mobile de la plaque
+%hypothÃ¨se  : seule la force normale est considÃ©rÃ©e comme prÃ©pondÃ©rante sur
 %les autres
 
 
 Fb = 0.5*rhoair*L*l*(Ub(1).^2+Ub(2).^2+Ub(3).^2).*[0;0;CN_3D(q1,q2,q3,q4,u,v,w,vf,vyf,wf,Vq)]; 
 
-%Forces dans le référentiel fixe :
+%Forces dans le rÃ©fÃ©rentiel fixe :
 
-Fi=transpose(Rq(q1,q2,q3,q4))*Fb;
+Fi=transpose(Matrice_de_passage(q1,q2,q3,q4))*Fb;
 
 end
