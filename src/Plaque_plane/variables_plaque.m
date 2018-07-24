ffd09b8e4b84a5c3ea8d7eb9012fc6f965ff1a26
easwaray <40615078+easwaray@users.noreply.github.com>
2018-07-25 00:55:47 +0200

    Update variables_plaque.m

--- src/Plaque_plane/variables_plaque.m
+++ src/Plaque_plane/variables_plaque.m
@@ -1,26 +1,26 @@
-filename='Clip_BWB08.csv'; %Fichier CFD contenant les données de l'écoulement
-B = csvread('Surface_BWB.csv',1,0); %Fichier contenant les coordonnées des points de la surface de l'avion
-BWB=alphaShape(B(:,24), B(:,25), B(:,26),2); %Définition de l'AlphaShape 
-method = 'linear'; %linear, nearest ou natural. Méthode pour l'interpolation des vitesses
+filename='Clip_BWB08.csv'; %Fichier CFD contenant les donnÃ©es de l'Ã©coulement
+B = csvread('Surface_BWB.csv',1,0); %Fichier contenant les coordonnÃ©es des points de la surface de l'avion
+BWB=alphaShape(B(:,24), B(:,25), B(:,26),2); %DÃ©finition de l'AlphaShape 
+method = 'linear'; %linear, nearest ou natural. MÃ©thode pour l'interpolation des vitesses
 choix_moment = 2 ; % 1 : Richards; 2 : Tachikawa
 X_Plan = 10; %Abscisse (m) du plan de normale x dans lequel on veut visualiser l'intersection avec les trajectoires
 SR=1; %1,2 Surface Ratio de la plaque
 L=0.1;  % Longueur de la plaque (m)   
-l=L/SR ; %Largeur de la plaque (m), calculée automatiquement avec le SR    
-e=0.002; %épaisseur de la plaque (m)
-g=9.81;  %gravité (m/s2)
+l=L/SR ; %Largeur de la plaque (m), calculÃ©e automatiquement avec le SR    
+e=0.002; %Ã©paisseur de la plaque (m)
+g=9.81;  %gravitÃ© (m/s2)
 rhoShimoi=917.057; %masse volumique de la glace (kg/m3)
 mass=rhoShimoi*L*l*e; %masse de la plaque (kg)
 rhoair=1.195; %masse volumique de l'air (kg/m3)
 
-% angles = [30;20;30]; %angles d'Euler initiaux (repère avion, rotations autour de z, y et x) en degrés
-% [q10,q20,q30,q40]=Euler2quat(angles(1)*pi/180,-angles(2)*pi/180,-angles(3)*pi/180); %quaternions initiaux
-% %Conditions initiales : [P0 Q0 R0 q10 q20 q30 q40  x0 u0  y0 v0 z0 w0      phi0               theta0             psi0]
-% Xin=[ 0, 0, 0,q10,q20,q30,q40, 0, 0,  6, 0, 1, 0, angles(1)*pi/180, -angles(2)*pi/180, -angles(3)*pi/180];
+ angles = [30;20;30]; %angles d'Euler initiaux (repÃ¨re avion, rotations autour de z, y et x) en degrÃ©s
+ [q10,q20,q30,q40]=quaternions(angles(1)*pi/180,-angles(2)*pi/180,-angles(3)*pi/180); %quaternions initiaux
+ %Conditions initiales : [P0 Q0 R0 q10 q20 q30 q40  x0 u0  y0 v0 z0 w0      phi0               theta0             psi0]
+ Xin=[ 0, 0, 0,q10,q20,q30,q40, 0, 0,  6, 0, 1, 0, angles(1)*pi/180, -angles(2)*pi/180, -angles(3)*pi/180];
 
 %temps de simulation
 tmax=1;
-%nous forçons l'écriture à un intervalle deltat
+%nous forÃ§ons l'Ã©criture Ã  un intervalle deltat
 %pas de temps
 deltat=0.001;
 
@@ -32,7 +32,7 @@ Izz=1/12*mass*(l^2+L^2);
 Iplate=diag([Ixx; Iyy; Izz]); % matrice d'inertie de la plaque
 invIplate=inv(Iplate);  % inverse de la matrice d'inertie
 
-% Choix de la base de donnée de Richards pour avoir le CN correspondant au
+% Choix de la base de donnÃ©e de Richards pour avoir le CN correspondant au
 % SR choisi
 if SR == 1
     data_CN = importdata('sr1.txt');
@@ -42,8 +42,8 @@ elseif SR == 4
     data_CN = importdata('sr4.txt');
 end
 
-alp = data_CN(:,1); % angle d'attaque de l'écoulement
-bet = data_CN(:,3); % angle d'inclinaison de l'écoulement 
+alp = data_CN(:,1); % angle d'attaque de l'Ã©coulement
+bet = data_CN(:,3); % angle d'inclinaison de l'Ã©coulement 
 
 Cn = data_CN(:,2);
 
